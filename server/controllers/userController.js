08e59fbe184799781d737c416c890551a01c12f3
anoopraju31 <anoopraju31@gmail.com>
2023-07-10 21:22:15 +0530

    Server: User Route prevent registering user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,6 +23,13 @@ const registerUser = asyncHandler(async (req, res) => {
 		throw new Error(errorText)
 	}
 
+	const userExists = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
 	const user = await User.create({ name, email, password })
 	res.status(200).json({ user })
 })
