27575fde44202eac23cb5a72d80d0823a9abc126
anoopraju31 <anoopraju31@gmail.com>
2023-07-10 15:39:16 +0530

    Server: async handler added to portfolio controler

--- server/controllers/portfolioControllers.js
+++ server/controllers/portfolioControllers.js
@@ -1,22 +1,24 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get all portfolio
 // @route GET /api/portfolio
 // @access for time being public
-const getPortfolio = (req, res) => {
+const getPortfolio = asyncHandler((req, res) => {
 	res.status(200).json({ message: 'portfolio get' })
-}
+})
 
 // @desc Get a specific token from portfolio
 // @route GET /api/portfolio/:id
 // @access for time being public
-const getPortfolioByToken = (req, res) => {
+const getPortfolioByToken = asyncHandler((req, res) => {
 	const id = req.params.id
 	res.status(200).json({ message: `portfolio get ${id}` })
-}
+})
 
 // @desc Add a token to portfolio
 // @route POST /api/portfolio
 // @access for time being public
-const addToPortfolio = (req, res) => {
+const addToPortfolio = asyncHandler((req, res) => {
 	const { token, quantity, invested } = req.body
 	// console.log(token, quantity, invested)
 
@@ -26,23 +28,23 @@ const addToPortfolio = (req, res) => {
 	}
 
 	res.status(200).json({ message: 'portfolio post' })
-}
+})
 
 // @desc Edit a token from portfolio
 // @route PUT /api/portfolio/:id
 // @access for time being public
-const editPortfolio = (req, res) => {
+const editPortfolio = asyncHandler((req, res) => {
 	const id = req.params.id
 	res.status(200).json({ message: `portfolio put ${id}` })
-}
+})
 
 // @desc Delete a token from portfolio
 // @route PUT /api/portfolio/:id
 // @access for time being public
-const deleteFromPortfolio = (req, res) => {
+const deleteFromPortfolio = asyncHandler((req, res) => {
 	const id = req.params.id
 	res.status(200).json({ message: `portfolio delete ${id}` })
-}
+})
 
 module.exports = {
 	getPortfolio,
