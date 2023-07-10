0dfce75006b6066eaa341f6a6dbcbe505dae72b1
anoopraju31 <anoopraju31@gmail.com>
2023-07-10 14:17:00 +0530

    Server: custom error handler initial

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const bodyparser = require('body-parser')
+const errorHandler = require('./middlewares/errorHandler.js')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,6 +10,7 @@ app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
 app.use('/api/portfolio', require('./routes/portfolioRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => {
 	console.log(`server running on port ${port}`)
