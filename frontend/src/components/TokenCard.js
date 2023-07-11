9fffadd6eac95e7c90857e57925489bdbf052010
anoopraju31 <anoopraju31@gmail.com>
2023-07-11 17:38:09 +0530

    Frontent: Token card component initial

---
+++ frontend/src/components/TokenCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TokenCard = () => {
+	return <div>TokenCard</div>
+}
+
+export default TokenCard
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar'
 export { default as Stat } from './Stat'
+export { default as TokenCard } from './TokenCard'
