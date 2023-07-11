23f18cb9b48f49f2b0d93e20e3a60233f5013651
anoopraju31 <anoopraju31@gmail.com>
2023-07-11 17:07:46 +0530

    Frontent: Stat component initial

---
+++ frontend/src/components/Stat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Stat = () => {
+	return (
+		<div className='flex flex-col items-center'>
+			<div className='flex justify-between'>
+				<h3 className='font-medium'> Current Value </h3>
+			</div>
+
+			<div className='my-2'>
+				<h2 className='text-4xl font-bold'> USD 24,456 </h2>
+			</div>
+		</div>
+	)
+}
+
+export default Stat
