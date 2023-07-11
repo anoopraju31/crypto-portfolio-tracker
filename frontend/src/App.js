d52202be4af6b7ffe2a49e77e5e4911c8bd08382
anoopraju31 <anoopraju31@gmail.com>
2023-07-11 15:03:02 +0530

    Frontend: Login page initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Login } from './pages'
 
 const App = () => {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+	return <Login />
 }
 
 export default App
