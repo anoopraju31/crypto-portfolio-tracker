e31d6816a2c0e80af9189414036b797028f581aa
anoopraju31 <anoopraju31@gmail.com>
2023-07-11 16:11:23 +0530

    Frontent: Navbar componenent styled

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<div className='container mx-auto py-2 px-4 flex justify-between items-center'>
+			<h1 className='text-lg font-medium'> Crypto </h1>
+
+			<div className='relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full '>
+				<span className='font-medium text-gray-600'>JL</span>
+			</div>
+		</div>
+	)
 }
 
 export default Navbar
